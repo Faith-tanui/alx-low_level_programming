d2be540e20d2cbff673f67f8943600d9900dae6f
Faith-tanui <tishtanui7980@gmail.com>
2022-12-08 15:50:11 -0800

    New update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,6 @@ int main(void)
  * Return: 0 (Means the program is a success)
  */
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+    	return(0);
 }

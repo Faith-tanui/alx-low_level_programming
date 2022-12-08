670f54dedc0e0d244ba62a1a7772cd78e919c913
Faith-tanui <tishtanui7980@gmail.com>
2022-12-08 15:45:36 -0800

    New update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
+int main(void)
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: 0 (Means the program is a success)
  */
-int main()
 {
     puts("\"Programming is like building a multilingual puzzle");
     return(0);
-{
 }

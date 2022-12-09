13bf56b7de26a9f35b53b4dba4cb054ba28c51d5
Faith-tanui <tishtanui7980@gmail.com>
2022-12-08 16:10:44 -0800

    The use of printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print out a given string using printf
+ *
+ * Return: 0 means program was succesful.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

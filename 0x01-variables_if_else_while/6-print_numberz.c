c5755a83cc3cbc915d58e1ffadfb074b174e9099
Faith-tanui <tishtanui7980@gmail.com>
2022-12-13 05:10:31 -0800

    Single digit numbers of base 10

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-	char ci;
+	char ch;
 
-	for (c = '0'; c <= '9'; c++)
+	for (ch = '0'; ch <= '9'; ch++)
 	{
-		putchar(c);
+		putchar(ch);
 	}
 	putchar('\n');
 	return (0);

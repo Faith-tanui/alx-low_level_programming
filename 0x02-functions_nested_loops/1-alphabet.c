fa735e0b0b24570042a74f83f4442b6a099dbe26
Faith-tanui <tishtanui7980@gmail.com>
2022-12-14 05:56:04 -0800

    Print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * print_alphabet - print the alphabets in lower case
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
+

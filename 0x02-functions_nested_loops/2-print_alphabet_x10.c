29141aaddcc95ce79b1e2f04aa9bb4a753c40ed4
Faith-tanui <tishtanui7980@gmail.com>
2022-12-18 13:59:15 -0800

    Printing the alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,7 @@
  * main - Entry point
  * Description -printing out the alphabet 10 times in lower case
  * Return: 0
- * print_alphabet_x10() -prints the alphabet 10 times in lowercase
+ * print_alphabet_x10(): prints the alphabet 10 times in lowercase
  *
  */
 void print_alphabet_x10(void)

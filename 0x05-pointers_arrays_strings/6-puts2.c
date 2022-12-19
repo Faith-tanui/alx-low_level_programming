b0f5b557156ee6c627890bcca5055897c05cc117
Faith-tanui <tishtanui7980@gmail.com>
2022-12-19 13:26:46 -0800

    Print every character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * puts2 - function that prints all characters of a string
+ * @str: parameter to check
+ * Return: 0
+ */
+void puts2(char *str)
+{
+	int string;
+
+	for (string = 0; str[string] != '\0'; string++)
+	if (string % 2 == 0)
+		_putchar(str[string]);
+	_putchar('\n');
+}

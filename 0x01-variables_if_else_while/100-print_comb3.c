868bfb1ebe875516f926620e10d851b9c804dd15
Faith-tanui <tishtanui7980@gmail.com>
2023-02-04 10:11:21 -0800

    Smallest single digit combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 is always succesful
+ */
+
+int main(void)
+{
+	int num1;
+	int num2;
+	char ch = ',';
+	char space = ' ';
+
+	for (num1 = 0; num1 <= 8; num1++)
+	{
+
+		for (num2 = 1; num2 <= 9; num2++)
+
+			if (num1 < num2)
+
+			{
+
+				putchar(num1 + '0');
+
+				putchar(num2 + '0');
+
+				putchar(ch);
+
+				putchar(space);
+
+
+			}
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
